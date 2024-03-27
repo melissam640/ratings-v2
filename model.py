7cc2e6f58b12aea046c826ae510dee7f7d64e9e9
Melissa McGrath <melissam640@gmail.com>
2024-03-27 16:46:54 -0700

    created seed_database.py

--- model.py
+++ model.py
@@ -60,7 +60,7 @@ class Rating(db.Model):
 
 def connect_to_db(flask_app, db_uri="postgresql:///ratings", echo=True):
     flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
-    flask_app.config["SQLALCHEMY_ECHO"] = echo
+    # flask_app.config["SQLALCHEMY_ECHO"] = echo
     flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.app = flask_app
